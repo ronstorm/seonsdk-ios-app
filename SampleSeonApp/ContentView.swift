78cb6aca2cdd7c4fd6c82d77d1805518277ec116
Amit Sen <amit.ron.cse@gmail.com>
2024-09-16 22:36:29 +0200

    Initial Commit

---
+++ SampleSeonApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SampleSeonApp
+//
+//  Created by Amit on 16.09.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SampleSeonApp/SampleSeonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleSeonAppApp.swift
+//  SampleSeonApp
+//
+//  Created by Amit on 16.09.24.
+//
+
+import SwiftUI
+
+@main
+struct SampleSeonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
